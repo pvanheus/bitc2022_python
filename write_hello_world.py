66c1912e823a8becf979d4e654d628864a07812d
Peter van Heusden <pvh@sanbi.ac.za>
2022-04-08 11:56:21 +0200

    Up to the end of chapter 4

--- write_hello_world.py
+++ write_hello_world.py
@@ -2,6 +2,7 @@ my_file = open("out.txt", "w")
 my_file.write("Hello World\n")
 my_file.close()
 
+# Ctrl-/ to comment or uncomment code
 my_file = open("out.txt")
 my_file_contents = my_file.read()
 print(my_file_contents)
